6ffc32671d314532dbdc242cfaff252175bf39ec
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-08 23:35:22 +0100

    work in progress: migrate to ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
-import { NgReduxRouterModule, NgReduxRouter } from '@angular-redux/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookComponent } from './book/book.component';
-import { CreateBookComponent } from './create-book/create-book.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { BookStoreService } from './shared/book-store.service';
-
-import { rootReducer } from './_reducers/rootReducer';
-import { IAppState } from './_reducers/types';
-import { CounterActions } from './_actions/counter.action';
-import { BooksActions } from './_actions/books.action';
+import { BookComponent } from './book/book.component';
 import { CounterComponent } from './counter/counter.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookStoreService } from './shared/book-store.service';
+import { CustomRouterStateSerializer } from './shared/utils';
 
 @NgModule({
   declarations: [
@@ -33,31 +32,57 @@ import { CounterComponent } from './counter/counter.component';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    NgReduxModule,
-    NgReduxRouterModule
+
+    /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.forRoot(reducers, { metaReducers }),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store.
+     */
+    StoreRouterConnectingModule,
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+
+    /**
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
+     */
+    EffectsModule.forRoot([])
   ],
   providers: [
     BookStoreService,
-    DevToolsExtension,
-    CounterActions,
-    BooksActions
+
+    /**
+     * The `RouterStateSnapshot` provided by the `Router` is a large complex structure.
+     * A custom RouterStateSerializer is used to parse the `RouterStateSnapshot` provided
+     * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
+     */
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 
-  constructor(
-    ngRedux: NgRedux<IAppState>,
-    ngReduxRouter: NgReduxRouter,
-    devTools: DevToolsExtension) {
-
-    ngRedux.configureStore(
-      rootReducer,
-      {},
-      [],
-      devTools.isEnabled() ? [devTools.enhancer()] : []
-    );
-
-    ngReduxRouter.initialize();
-  }
+  // constructor(booksActions: BooksActions) {
+  //   booksActions.loadBooks();
+  // }
 }
