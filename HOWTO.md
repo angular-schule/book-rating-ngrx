55f39fec778907452194536dfda85457b6b2f073
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-10-21 14:20:58 +0200

    Initial State Setup + Initial Effects Setup

--- HOWTO.md
+++ HOWTO.md
@@ -30,13 +30,8 @@ npm install @ngrx/store @ngrx/effects @ngrx/store-devtools @ngrx/router-store --
 Generate the initial state management and register it within the `app.module.ts`
 
 ```sh
-ng generate @ngrx/schematics:store State --root --module app.module.ts --statePath store/reducers 
+ng generate @ngrx/schematics:store State --root --module app.module.ts
 ```
-
-We create the folder `store` here, because we don't want to have our reduces at app-level.
-The same folder structure is used in the official NgRx demo.
-
-Unfortunately the schematics isn't perfect. We have to open `app.module.ts` and fix the import to the `environment.ts ` 
 
 ## 4. Initial Effects Setup
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookComponent } from './book/book.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookStoreService } from './shared/book-store.service';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,10 @@ import { BookStoreService } from './shared/book-store.service';
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [
     BookStoreService
